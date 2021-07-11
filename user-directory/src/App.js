7be4f5e48341a0ac372a4263d95ed1357fe33bff
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-07-11 19:49:00 -0400

    install axios

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import "/App.css";
+import EmployeeCard from './components/EmployeeCard';
+import Wrapper from './components/Wrapper';
+//import Col from './components/Col';
+import './App.css';
 
 
-export default function App() {
-  return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  );
+    );
+  }
 }
